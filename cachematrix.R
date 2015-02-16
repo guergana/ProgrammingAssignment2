b138eb1b28ed9acd738a9f941e8f4ef9caa9169d
Guergana <pestanias@yahoo.com>
2015-02-16 16:02:16 -0600

    solution to exercise

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  minv <- NULL
+  set <- function(y) {
+    x <- y
+    minv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inverse) minv <<- solve(inverse)
+  getInv <- function() minv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  minv <- x$getInv()
+  if(!is.null(minv)) {
+    message("getting cached data")
+    return(minv)
+  }
+  data <- x$get()
+  minv <- solve(data, ...)
+  x$setInv(minv)
+  minv
 }
